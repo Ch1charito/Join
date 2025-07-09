79895fc449962e7deabb9bb954153a16989c5a19
Muhammad <m.shaafan@outlook.de>
2025-07-09 11:51:43 +0200

    overlay

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
-import { SideBarComponent } from "../shared/side-bar/side-bar.component";
+import { Component, ViewChild } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { SideBarComponent } from '../shared/side-bar/side-bar.component';
+import { AddContactOverlayComponent } from './add-contact-overlay/add-contact-overlay.component';
+import { EditContactOverlayComponent } from './edit-contact-overlay/edit-contact-overlay.component';
 
 @Component({
   selector: 'app-contacts',
-  imports: [HeaderComponent, SideBarComponent],
+  imports: [
+    HeaderComponent,
+    SideBarComponent,
+    AddContactOverlayComponent,
+    EditContactOverlayComponent,
+  ],
   templateUrl: './contacts.component.html',
-  styleUrl: './contacts.component.scss'
+  styleUrl: './contacts.component.scss',
 })
 export class ContactsComponent {
+  // @ViewChild('addContact') private addContact!: AddContactOverlayComponent;
+  showOverlay: boolean = false;
 
+  openAddContact() {
+    this.showOverlay = !this.showOverlay;
+  }
 }

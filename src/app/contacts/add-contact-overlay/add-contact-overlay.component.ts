76d2fd2336d8e1833b5de4abc40e2a3cbe2f772d
Ch1charito <r.buschmann.work@gmail.com>
2025-07-10 16:01:39 +0200

    added add function to database

--- src/app/contacts/add-contact-overlay/add-contact-overlay.component.ts
+++ src/app/contacts/add-contact-overlay/add-contact-overlay.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-contact-overlay',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-contact-overlay.component.html',
   styleUrl: './add-contact-overlay.component.scss'
 })
 export class AddContactOverlayComponent {
+  firebaseService = inject(FirebaseService);
+  contacts = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+
+  submitContact() {
+    this.firebaseService.addContactToDatabase(this.contacts);
+    this.clearInputFields();
+  }
+
+  clearInputFields() {
+    this.contacts.name = '';
+    this.contacts.email = '';
+    this.contacts.phone = '';
+  }
+
  
+  //#region overlay
   @Output() close = new EventEmitter<void>();
 
   onCloseClick() {
     this.close.emit();
   }
+  //#endregion
 }  

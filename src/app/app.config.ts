25c60f13141f44f2b82c347988002b206c4ef9d8
Adem <ado.designs@outlook.de>
2025-07-10 13:44:50 +0200

    app.config file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,5 +10,17 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-80a7b',
+        appId: '1:789977677975:web:ec6096e9bd795ea0b029d2',
+        storageBucket: 'join-80a7b.firebasestorage.app',
+        apiKey: 'AIzaSyB46EgjrM_9F696WOt0EgUo9116ZWhHFPs',
+        authDomain: 'join-80a7b.firebaseapp.com',
+        messagingSenderId: '789977677975',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };

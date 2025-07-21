2636b906b17c7da9820845fd3c232b75ca32ee32
Ch1charito <r.buschmann.work@gmail.com>
2025-07-21 12:48:45 +0200

    added priority and assigned selected contacts to add task form

--- src/app/interfaces/task.interface.ts
+++ src/app/interfaces/task.interface.ts
@@ -1,6 +1,10 @@
+import { ContactInterface } from "./contact.interface";
+
 export interface TaskInterface{
     id?: string;
     title: string;
     description: string;
     date: string;
+    priority: string;
+    assignedContacts: ContactInterface[];
 }

2d6279bafd06cced307853b8e9031a22be2ebb11
Ch1charito <r.buschmann.work@gmail.com>
2025-07-23 17:04:31 +0200

    added function for drag and drop aswell as function to update database

--- src/app/board/board-bottom/board-bottom.component.ts
+++ src/app/board/board-bottom/board-bottom.component.ts
@@ -6,9 +6,11 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CardComponent } from "../card/card.component";
 import { CardOverlayComponent } from "../card-overlay/card-overlay.component";
+import { TaskInterface } from '../../interfaces/task.interface';
+import { FirebaseService } from '../../services/firebase.service';
 
 @Component({
   selector: 'app-board-bottom',
@@ -16,12 +18,47 @@ import { CardOverlayComponent } from "../card-overlay/card-overlay.component";
   templateUrl: './board-bottom.component.html',
   styleUrl: './board-bottom.component.scss'
 })
-export class BoardBottomComponent {
-  todo: string[] = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+export class BoardBottomComponent implements OnInit {
+  /* todo: string[] = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
   progress: string[] = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
   feedback: string[] = ['zu machen', 'lalala', 'banana'];
-  done: string[] = [];
-  drop(event: CdkDragDrop<string[]>) {
+  done: string[] = []; */
+
+  firebaseService = inject(FirebaseService);
+
+  todo: TaskInterface[] = [];
+  progress: TaskInterface[] = [];
+  feedback: TaskInterface[] = [];
+  done: TaskInterface[] = [];
+
+  ngOnInit() {
+    this.filterTasksByStatus();
+  }
+
+
+  /* filterTasksByStatus() {
+    this.todo = this.firebaseService.taskList.filter(task => task.status === 'todo');
+    this.progress = this.firebaseService.taskList.filter(task => task.status === 'progress');
+    this.feedback = this.firebaseService.taskList.filter(task => task.status === 'feedback');
+    this.done = this.firebaseService.taskList.filter(task => task.status === 'done');
+
+    console.log('Gefilterte Tasks:', {
+    todo: this.todo,
+    progress: this.progress,
+    feedback: this.feedback,
+    done: this.done
+  });
+  } */
+ filterTasksByStatus() {
+  setTimeout(() => {
+    this.todo = this.firebaseService.taskList.filter(task => task.status === 'todo');
+    this.progress = this.firebaseService.taskList.filter(task => task.status === 'progress');
+    this.feedback = this.firebaseService.taskList.filter(task => task.status === 'feedback');
+    this.done = this.firebaseService.taskList.filter(task => task.status === 'done');
+  }, 100); // Delay in ms – evtl. anpassen
+}
+
+  /* drop(event: CdkDragDrop<TaskInterface[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -31,6 +68,37 @@ export class BoardBottomComponent {
         event.previousIndex,
         event.currentIndex,
       );
+    }
+  } */
+
+  drop(event: CdkDragDrop<TaskInterface[]>) {
+  if (event.previousContainer === event.container) {
+    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+  } else {
+    // Task verschieben
+    const movedTask = event.previousContainer.data[event.previousIndex];
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex,
+    );
+
+    // Neuen Status bestimmen anhand des Zielcontainers
+    let newStatus = '';
+    switch (event.container.id) {
+      case 'todoList': newStatus = 'todo'; break;
+      case 'progressList': newStatus = 'progress'; break;
+      case 'feedbackList': newStatus = 'feedback'; break;
+      case 'doneList': newStatus = 'done'; break;
+    }
+
+    // Task updaten
+    movedTask.status = newStatus;
+    this.firebaseService.updateTaskInDatabase(movedTask.id!, movedTask);
+
+    // Neu filtern
+    this.filterTasksByStatus();
     }
   }
 

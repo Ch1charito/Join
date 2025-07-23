b65abd8bd82329d2754376080ec2dc18f1e32bb4
Ch1charito <r.buschmann.work@gmail.com>
2025-07-23 17:06:57 +0200

    edited some scss

--- src/app/board/board-bottom/board-bottom.component.ts
+++ src/app/board/board-bottom/board-bottom.component.ts
@@ -19,13 +19,7 @@ import { FirebaseService } from '../../services/firebase.service';
   styleUrl: './board-bottom.component.scss'
 })
 export class BoardBottomComponent implements OnInit {
-  /* todo: string[] = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  progress: string[] = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
-  feedback: string[] = ['zu machen', 'lalala', 'banana'];
-  done: string[] = []; */
-
   firebaseService = inject(FirebaseService);
-
   todo: TaskInterface[] = [];
   progress: TaskInterface[] = [];
   feedback: TaskInterface[] = [];
@@ -35,47 +29,19 @@ export class BoardBottomComponent implements OnInit {
     this.filterTasksByStatus();
   }
 
-
-  /* filterTasksByStatus() {
-    this.todo = this.firebaseService.taskList.filter(task => task.status === 'todo');
-    this.progress = this.firebaseService.taskList.filter(task => task.status === 'progress');
-    this.feedback = this.firebaseService.taskList.filter(task => task.status === 'feedback');
-    this.done = this.firebaseService.taskList.filter(task => task.status === 'done');
-
-    console.log('Gefilterte Tasks:', {
-    todo: this.todo,
-    progress: this.progress,
-    feedback: this.feedback,
-    done: this.done
-  });
-  } */
- filterTasksByStatus() {
-  setTimeout(() => {
-    this.todo = this.firebaseService.taskList.filter(task => task.status === 'todo');
-    this.progress = this.firebaseService.taskList.filter(task => task.status === 'progress');
-    this.feedback = this.firebaseService.taskList.filter(task => task.status === 'feedback');
-    this.done = this.firebaseService.taskList.filter(task => task.status === 'done');
-  }, 100); // Delay in ms – evtl. anpassen
-}
-
-  /* drop(event: CdkDragDrop<TaskInterface[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-    }
-  } */
+  filterTasksByStatus() {
+    setTimeout(() => {
+      this.todo = this.firebaseService.taskList.filter(task => task.status === 'todo');
+      this.progress = this.firebaseService.taskList.filter(task => task.status === 'progress');
+      this.feedback = this.firebaseService.taskList.filter(task => task.status === 'feedback');
+      this.done = this.firebaseService.taskList.filter(task => task.status === 'done');
+    }, 100); 
+  }
 
   drop(event: CdkDragDrop<TaskInterface[]>) {
   if (event.previousContainer === event.container) {
     moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
   } else {
-    // Task verschieben
     const movedTask = event.previousContainer.data[event.previousIndex];
     transferArrayItem(
       event.previousContainer.data,
@@ -83,8 +49,6 @@ export class BoardBottomComponent implements OnInit {
       event.previousIndex,
       event.currentIndex,
     );
-
-    // Neuen Status bestimmen anhand des Zielcontainers
     let newStatus = '';
     switch (event.container.id) {
       case 'todoList': newStatus = 'todo'; break;
@@ -92,12 +56,8 @@ export class BoardBottomComponent implements OnInit {
       case 'feedbackList': newStatus = 'feedback'; break;
       case 'doneList': newStatus = 'done'; break;
     }
-
-    // Task updaten
     movedTask.status = newStatus;
     this.firebaseService.updateTaskInDatabase(movedTask.id!, movedTask);
-
-    // Neu filtern
     this.filterTasksByStatus();
     }
   }

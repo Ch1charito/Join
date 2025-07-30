4ffbe3a316cabdc0f66a615f285faefeceb8f554
Muhammad <m.shaafan@outlook.de>
2025-07-30 15:27:40 +0200

    responsive add task überarbeiten

--- src/app/add-task/category/category.component.ts
+++ src/app/add-task/category/category.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; 
+import { Component, Input, Output, EventEmitter, HostListener, ViewChild } from '@angular/core'; 
+import { FormsModule, NgModel } from '@angular/forms'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category',
@@ -15,9 +15,10 @@ export class CategoryComponent {
   };
   @Input() category: string = '';
   @Output() taskListChange = new EventEmitter<any>();
-  showCategoryDropdown: boolean = false; 
+  showCategoryDropdown: boolean = false;
 
-  
+  @ViewChild('category') categoryNgModel!: NgModel;
+
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent): void {
     const categoryDiv = document.getElementById('categoryDiv');
@@ -30,27 +31,51 @@ export class CategoryComponent {
       !categoryDropdown.contains(event.target as Node)
     ) {
       this.showCategoryDropdown = false;
+    
+      if (this.categoryNgModel) {
+        this.categoryNgModel.control.markAsTouched();
+      }
     }
   }
 
   onCategoryChange() {
     this.taskListChange.emit(this.taskList.category);
+    
+    if (this.categoryNgModel) {
+      this.categoryNgModel.control.markAsTouched();
+    }
   }
 
   resetCategory() {
     this.taskList.category = '';
+
+    if (this.categoryNgModel) {
+      this.categoryNgModel.control.markAsUntouched();
+      this.categoryNgModel.control.markAsPristine();
+      this.categoryNgModel.control.updateValueAndValidity();
+    }
   }
 
-  
+
   toggleCategoryDropdown(): void {
     this.showCategoryDropdown = !this.showCategoryDropdown;
+    
+    if (this.categoryNgModel) {
+      this.categoryNgModel.control.markAsTouched();
+    }
   }
 
- 
+
   selectCategory(category: string): void {
     this.taskList.category = category;
-    this.onCategoryChange(); 
-    this.showCategoryDropdown = false; 
+    this.onCategoryChange();
+    this.showCategoryDropdown = false;
+    
+    if (this.categoryNgModel) {
+      this.categoryNgModel.control.markAsDirty();
+      this.categoryNgModel.control.markAsTouched();
+      this.categoryNgModel.control.updateValueAndValidity(); 
+    }
   }
 
   ngOnChanges() {

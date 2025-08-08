30e67fba7be607c4239eeb25ea3f21365b8e64c0
Adem <ado.designs@outlook.de>
2025-08-08 22:30:57 +0200

    feat(auth): create base structure for login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { ContactsComponent } from './contacts/contacts.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { SummaryComponent } from './summary/summary.component';
 import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: AddTaskComponent},
+    {path: '', component: LoginComponent},
+    {path: 'add-task', component: AddTaskComponent},
     {path: 'contacts', component: ContactsComponent},
     {path: 'summary', component: SummaryComponent},
     {path: 'board', component: BoardComponent},

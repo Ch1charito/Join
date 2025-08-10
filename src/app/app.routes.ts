8f2c1326d89a2ed0ae06e4938a6c84b131df25fc
Adem <ado.designs@outlook.de>
2025-08-10 20:10:54 +0200

    feat(auth): add routerLink to navigate from login to signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { AddTaskComponent } from './add-task/add-task.component';
 import { SummaryComponent } from './summary/summary.component';
 import { BoardComponent } from './board/board.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
+    {path: 'sign-up', component: SignUpComponent},
     {path: 'add-task', component: AddTaskComponent},
     {path: 'contacts', component: ContactsComponent},
     {path: 'summary', component: SummaryComponent},

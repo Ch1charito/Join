55f14c5f12e0d13ad0d29909dac20d3fa3c30875
Adem <ado.designs@outlook.de>
2025-08-11 17:46:37 +0200

    style(auth): add content limit for signup and login page

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -11,9 +11,9 @@ import { RouterLink } from '@angular/router';
 })
 export class SignUpComponent {
   submitted = false;
-  namePattern = "^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,}$";
+  namePattern = "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?:['--][A-Za-zÀ-ÖØ-öø-ÿ]+)*(?:\s+[A-Za-zÀ-ÖØ-öø-ÿ]+(?:['--][A-Za-zÀ-ÖØ-öø-ÿ]+)*)+$";
   passwordPattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s]).{8,}$";
-  emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+  emailPattern = "^[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+@(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,}$";
 
   formData = {
     name: '',

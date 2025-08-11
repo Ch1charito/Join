58079c1bcaa7dbafc5455d45d4332aa0e8f47a47
DevMinded <keles490@googlemail.com>
2025-08-11 12:29:26 +0200

    privacy policy + legal notice added

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../shared/side-bar/side-bar.component";
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [],
+  imports: [SideBarComponent, HeaderComponent],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss'
 })
--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../shared/side-bar/side-bar.component";
+import { HeaderComponent } from "../shared/header/header.component";
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [SideBarComponent, HeaderComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })

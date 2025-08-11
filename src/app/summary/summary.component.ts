ad951e3544cc1a2fd3d4802c481f3430704f1dfe
Ch1charito <r.buschmann.work@gmail.com>
2025-08-11 11:43:29 +0200

    added html and scss for summary

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../shared/header/header.component";
+import { SideBarComponent } from "../shared/side-bar/side-bar.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  imports: [HeaderComponent, SideBarComponent, RouterLink],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })

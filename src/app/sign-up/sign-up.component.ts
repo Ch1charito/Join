ae85932f71fe4f0141a0f600dd02be3da92a7955
Adem <ado.designs@outlook.de>
2025-08-13 01:25:25 +0200

    feat(auth): add form validation to signup using reactiveforms

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,42 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  signupForm;
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group(
+      {
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', [Validators.required]],
+        policy: [false, [Validators.requiredTrue]],
+  },
+  {
+    validators: [SignUpComponent.passwordsMatchValidator],
+  }
+);
+  }
+onSubmit() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
+    const { name, email } = this.signupForm.value;
+    console.log('loogg...', { name, email, password: '...' });
+  }
 
+  static passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const pw = group.get('password')?.value;
+    const cpw = group.get('confirmPassword')?.value;
+    return pw && cpw && pw !== cpw ? { passwordsMismatch: true } : null;
+  }
 }
+
